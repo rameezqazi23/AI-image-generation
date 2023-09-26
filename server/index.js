3861544af1e642e0e30332f90e4fb7179dfd7adb
rameezqazi23 <rameez.qazi23@gmail.com>
2023-09-26 22:11:13 +0500

    connection created with mongodb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import connectToMongoDb from "./mongodb/connect.js";
 const app = express();
 
+dotenv.config();
 
 const PORT = 8000;
+
+//middlewares
+app.use(cors());
+app.use(express.json({ limit: '50mb' }))
+
+
+//mongodb connection
+connectToMongoDb(process.env.MONGODB_URL)
+    .then(() => console.log("MongoDB Successfully connected!"))
+    .catch(() => console.log("MongoDB connection error"))
 
 
 app.get('/', (req, res) => {
